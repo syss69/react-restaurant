48856b7e30affd8ecc600799c56b98be5abc63cb
syss69 <art435sys@gmail.com>
2025-12-23 16:49:15 +0100

    Updates

--- _next/static/tkWOQ3FBa1c0wov2S9NfK/_buildManifest.js
+++ _next/static/tkWOQ3FBa1c0wov2S9NfK/_buildManifest.js
@@ -1,7 +1,12 @@
 self.__BUILD_MANIFEST = {
   "__rewrites": {
     "afterFiles": [],
-    "beforeFiles": [],
+    "beforeFiles": [
+      {
+        "source": "/react-restaurant//_next/:path+",
+        "destination": "/react-restaurant/_next/:path+"
+      }
+    ],
     "fallback": []
   },
   "sortedPages": [
