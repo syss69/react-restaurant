0564b8f7d4286dd2ff242b9805a44b6c75f92a1a
syss69 <art435sys@gmail.com>
2025-12-23 18:01:28 +0100

    Fix leaflet marker

--- src/components/Map.tsx
+++ src/components/Map.tsx
@@ -6,9 +6,9 @@ import L from 'leaflet'
 // icon fix (for next)
 delete (L.Icon.Default.prototype as any)._getIconUrl
 L.Icon.Default.mergeOptions({
-  iconRetinaUrl: '/leaflet/marker-icon-2x.png',
-  iconUrl: '/leaflet/marker-icon.png',
-  shadowUrl: '/leaflet/marker-shadow.png',
+  iconRetinaUrl: './leaflet/marker-icon-2x.png',
+  iconUrl: './leaflet/marker-icon.png',
+  shadowUrl: './leaflet/marker-shadow.png',
 })
 
 interface MapProps {
